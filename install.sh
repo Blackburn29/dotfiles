13b79ce114f9585c45f1dbfc413ea36c63c68df8
Eric Elsken <eric@pmg.co>
2014-10-18 12:23:16 -0500

    Updated install.sh for bashrc linking.

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 
 VIMRC_NAME="vimrc"
 
-cd ~
+cd "$HOME"
 ln -sf "$DIR/$VIMRC_NAME" .vimrc
-cd - > /dev/null
+
+if ! grep -q "source.*$DIR/bashrc" .bashrc; then
+	echo -e "\nsource $DIR/bashrc\n" >> .bashrc
+fi
+cd "$DIR" &> /dev/null
