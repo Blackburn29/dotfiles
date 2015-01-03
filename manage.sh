9798263beca8899f300f2ff6a376a6343e6b5585
Eric Elsken <eric@pmg.co>
2015-01-02 22:34:56 -0600

    Updated manage.sh to take arguments for things to configure.

--- manage.sh
+++ manage.sh
@@ -3,18 +3,37 @@
 #the following should be the first content line of the script.
 DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
 
-#things to do in the home directory.
-cd "$HOME"
-ln -sf "$DIR/vimrc" .vimrc
+manage_bash() {
+    #add a source line to ~/.bashrc to source this file if it does not already exist.
+    if ! grep -q "source.*$DIR/bashrc" ~/.bashrc; then
+        echo -e "\nsource $DIR/bashrc\n" >> ~/.bashrc
+    fi
+}
 
-if ! grep -q "source.*$DIR/bashrc" .bashrc; then
-	echo -e "\nsource $DIR/bashrc\n" >> .bashrc
-fi
-cd "$DIR" &> /dev/null
-#done with things in the home directory.
+manage_vim() {
+    vimdirs=( plugin ftplugin ftdetect indent )
+    for vimdir in ${vimdirs[@]}; do
+        mkdir -p "$DIR/vim/$vimdir"
+        ln -sf "$DIR/vim/$vimdir" ~/.vim
+    done
 
-#clone and install Vundle for vim.
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    #clone and install Vundle for vim.
+    if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+        mkdir -p ~/.vim/bundle
+        git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    fi
+    vim +PluginInstall +qall
+}
+
+args="$@"
+if [ "$#" -eq 0 ]; then
+    args=( bash vim )
 fi
-vim +PluginInstall +qall
+for arg in ${args[@]}; do
+    case "$arg" in
+        bash)
+            manage_bash;;
+        vim)
+            manage_vim;;
+    esac
+done
