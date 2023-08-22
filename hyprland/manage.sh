f2af249de3d2496380089fb4876b3c7fb2705790
Blake LaFleur <blake.k.lafleur@gmail.com>
2023-08-21 22:47:39 -0500

    Add hyprload support

--- hyprland/manage.sh
+++ hyprland/manage.sh
@@ -12,7 +12,12 @@ HYPRLAND_CONFIG="$HYPRLAND_CONFIG_DIR/hyprland.conf"
 
 mkdir -p "$HYPRLAND_CONFIG_DIR"
 
+if [ ! -d "$HOME/.local/share/hyprload/" ]; then
+    curl -sSL https://raw.githubusercontent.com/Duckonaut/hyprload/main/install.sh | bash
+fi
+
 if [ ! -h "$HYPRLAND_CONFIG" ]; then
     ln -fs "$DIR/hyprland.conf" "$HYPRLAND_CONFIG_DIR"
     ln -fs "$DIR/xdg-portal-hyprland" "$HYPRLAND_CONFIG_DIR"
+    ln -fs "$DIR/hyprload.toml" "$HYPRLAND_CONFIG_DIR"
 fi
